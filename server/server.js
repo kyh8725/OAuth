d9614ebc14228f21d0df8e20f13da789b13d5610
Daniel Kim <kyh8725@hotmail.com>
2020-10-29 03:36:14 -0700

    path join

--- server/server.js
+++ server/server.js
@@ -116,7 +116,7 @@ mongoose.connect(MONGO_URL, {
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("../client/build"));
+  app.use(express.static(path_join(__dirname, "../client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
