8cc21c133bc44005daea07da95b904b12a175a86
Daniel Kim <kyh8725@hotmail.com>
2020-10-29 03:04:07 -0700

    start concurrently

--- server/server.js
+++ server/server.js
@@ -117,6 +117,10 @@ mongoose.connect(MONGO_URL, {
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("../client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+  });
 }
 
 mongoose.connection.on("connected", () => {
